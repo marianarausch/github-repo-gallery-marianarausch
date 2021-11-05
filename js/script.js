ac0405b155be082f742bdd7811936ba492195d20
Mariana Rausch <anzolmariana@gmail.com>
2021-11-05 15:15:44 +0100

    Added names of the repos

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 const overview = document.querySelector(".overview");
 const username = "marianarausch";
+const repoList = document.querySelector(".repo-list");
 
 //Fetch API JSON Data
 const gitUserInfo = async function () {
@@ -26,6 +27,23 @@ const displayUserInfo = function (data) {
   <p><Number of public repos:</strong>${data.public_repos}</p>
   `;
   overview.append(div);
+  fetchRepos();
 };
 
-// Call the display Function & view your project
+// Fetch repos
+const fetchRepos = async function () {
+  const listFetch = await fetch(
+    `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
+  );
+  const repoData = await listFetch.json();
+  displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
+};
